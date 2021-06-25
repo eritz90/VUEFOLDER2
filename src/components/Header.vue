bb36f6af302e15b0e1c685367a119bc6eee814c6
eritz90 <46261768+eritz90@users.noreply.github.com>
2021-06-25 15:15:13 +0800

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+      <h1>{{ title }}</h1>
+      <h1>This is a Newsfeed :)</h1>
+      <Button text ="Add Task" color = "green"/>
+      <Button text ="Edit Task" color = "blue"/>
+      <Button text ="Delete Task" color = "red"/>
+    </header>
+  </template>
+
+<script lang = "ts">
+import Button from '@/components/Button.vue';
+
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+  <style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3> {{ task.text }}</h3>
+</template>
+
+<script lang = "ts">
+    export default {
+        props: {
+            task: Object
+        }
+    }
+</script>
